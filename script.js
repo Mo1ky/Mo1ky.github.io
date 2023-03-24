7821aa8c777d22bacf9b69b0669f7e94066967eb
Mo1ky <119064533+Mo1ky@users.noreply.github.com>
2023-03-24 15:05:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,77 +12,79 @@ const body = document.querySelector('body'),
 /* =========================== */
 
 /* ===== Переключение бокового меню на закрытое или открытое ===== */
-toggle.addEventListener("click" , () =>{
+toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 })
 /* =========================== */
 
 /* ===== Быстрое перемещение в начало ===== */
 var t;
+
 function up() {
     console.log("dasds")
-	var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-	if(top > 0) {
-		window.scrollBy(0,-30);
-		t = setTimeout('up()',1);
-	} else clearTimeout(t);
-	return false;
+    var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if (top > 0) {
+        window.scrollBy(0, -30);
+        t = setTimeout('up()', 1);
+    } else clearTimeout(t);
+    return false;
 }
 /* =========================== */
 
 /* ===== Переключение бокового меню на закрытое или открытое в мобильной версии ===== */
-var x = window.matchMedia("(max-width: 850px)")
+var x = window.matchMedia("(max-width: 800px)")
+
 function myFunction(x) {
     if (x.matches) {
         sidebar.classList.add("mobile-close");
         sidebar.classList.remove("mobile-open");
 
-        mobiletoggle.addEventListener("click", () =>{
-            
+        mobiletoggle.addEventListener("click", () => {
+
             sidebar.classList.remove("close");
             sidebar.classList.remove("mobile-close");
             sidebar.classList.add('mobile-open');
 
-            closemobile.addEventListener("click" , () =>{
-                sidebar.classList.remove("mobile-open");  
-                sidebar.classList.add("mobile-close");   
-            })
-
-                general.addEventListener("click" , () =>{
-                sidebar.classList.remove("mobile-open");  
+            closemobile.addEventListener("click", () => {
+                sidebar.classList.remove("mobile-open");
                 sidebar.classList.add("mobile-close");
             })
-        }) 
-       
 
-    } 
+            general.addEventListener("click", () => {
+                sidebar.classList.remove("mobile-open");
+                sidebar.classList.add("mobile-close");
+            })
+        })
 
-    else {
+
+    } else {
         sidebar.classList.remove("mobile-close");
         sidebar.classList.remove("mobile-open");
     }
-    }
-    myFunction(x) 
-    x.addListener(myFunction)
+}
+myFunction(x)
+x.addListener(myFunction)
 /* ===========================*/
 
-/* ===== Поиск ===== */ 
+/* ===== Поиск ===== */
 
-searchBtn.addEventListener("click" , () =>{
-sidebar.classList.remove("close");}) 
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+})
 /* =========================== */
 
 /* ===== Переключение темы ===== */
 
-modeSwitch.addEventListener("click" , () =>{
-body.classList.toggle("dark");
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
 
-if(body.classList.contains("dark")){
-    modeText.innerText = "Светлая тема";
-}
-else{
-    modeText.innerText = "Тёмная тема";
-}});
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Светлая тема";
+    } else {
+        modeText.innerText = "Тёмная тема";
+    }
+});
 /* =========================== */
+/* ===== Галерея ===== */
 
-
+baguetteBox.run('.lightbox,.lightbox-group');
